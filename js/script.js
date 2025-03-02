a6ae3599a1fd91f4aa85c30d684b646fc1671850
Daniel <positivevibrationinfinity@gmail.com>
2025-03-02 10:26:21 +0300

    Initial commit for GreenFertix

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("GreenFertix website loaded successfully.");
+    // Additional JavaScript interactivity can be added here
+  });
